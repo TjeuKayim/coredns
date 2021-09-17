9a990a13393a55dcf1f14c93f2c9519671e801e1
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2021-09-17 12:59:05 +0000

    Implement echo

--- plugin/tunnel/whoami.go
+++ plugin/tunnel/whoami.go
@@ -4,8 +4,6 @@ package tunnel
 
 import (
 	"context"
-	"net"
-	"strconv"
 
 	"github.com/coredns/coredns/request"
 
@@ -26,30 +24,9 @@ func (wh Tunnel) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 	a.SetReply(r)
 	a.Authoritative = true
 
-	ip := state.IP()
-	var rr dns.RR
-
-	switch state.Family() {
-	case 1:
-		rr = new(dns.A)
-		rr.(*dns.A).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeA, Class: state.QClass()}
-		rr.(*dns.A).A = net.ParseIP(ip).To4()
-	case 2:
-		rr = new(dns.AAAA)
-		rr.(*dns.AAAA).Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeAAAA, Class: state.QClass()}
-		rr.(*dns.AAAA).AAAA = net.ParseIP(ip)
-	}
-
-	srv := new(dns.SRV)
-	srv.Hdr = dns.RR_Header{Name: "_" + state.Proto() + "." + state.QName(), Rrtype: dns.TypeSRV, Class: state.QClass()}
-	if state.QName() == "." {
-		srv.Hdr.Name = "_" + state.Proto() + state.QName()
-	}
-	port, _ := strconv.Atoi(state.Port())
-	srv.Port = uint16(port)
-	srv.Target = "."
-
-	a.Extra = []dns.RR{rr, srv}
+	rr := new(dns.CNAME)
+	rr.Hdr = dns.RR_Header{Name: state.QName(), Rrtype: dns.TypeCNAME, Class: state.QClass()}
+	rr.Target = state.QName() + "local."
 
 	w.WriteMsg(a)
 
